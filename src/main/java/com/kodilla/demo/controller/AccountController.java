aabd72e3380df8c7c831e21d0310db4942f32829
sobek21 <mateuszsobkowicz211@gmail.com>
2021-05-17 20:28:41 +0200

    add actuator

--- src/main/java/com/kodilla/demo/controller/AccountController.java
+++ src/main/java/com/kodilla/demo/controller/AccountController.java
@@ -6,12 +6,20 @@ import com.kodilla.demo.mapper.AccountMapper;
 import com.kodilla.demo.repository.AccountRepository;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-
+@RefreshScope
+@Slf4j
 @RestController
+@RequestMapping("/v1/accounts")
 @RequiredArgsConstructor
 public class AccountController {
 
@@ -19,11 +27,18 @@ public class AccountController {
 
   private final AccountMapper accountMapper;
 
+  @Value("${application.allow-get-accounts}")
+  private boolean allowGetAccounts;
 
-  @GetMapping("/v1/accounts")
-  public AccountDto getAccounts(@RequestParam long ConsumerID) {
-    Optional<Account> account = accountRepository.findById(ConsumerID);
-    return accountMapper.accountToAccountDto(account.get());
+
+  @GetMapping
+  public AccountDto getAccounts(@RequestParam("customerId") long customerId) {
+    if(!allowGetAccounts) {
+    log.info("Getting accounts is disabled");
+    throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "Getting accounts is disabled");
+    }
+    Account account = new Account();
+    return accountMapper.accountToAccountDto(account);
   }
 }
 
